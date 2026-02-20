7756cbd682669142730acff66757ae2192ae86b5
Jomar Milan <jomarm@jomarm.com>
2026-02-20 14:37:25 -0800

    sseedit: init at 4.1.5f

--- default.nix
+++ default.nix
@@ -1,8 +1,16 @@
-{
-  pkgs ? import <nixpkgs> {},
+args@{
+  sources ? import ./npins,
+  pkgs ? import sources.nixpkgs args,
   lib ? pkgs.lib,
+  erosanix ? import (fetchTarball "https://github.com/rhinofi/flake-compat/archive/55cdfe245f739b2b33d2037589fa7e0a0376efa4.tar.gz") { src = sources.erosanix; impureOverrides = { nixpkgs = sources.nixpkgs; }; },
+  # erosanix ? import sources.erosanix { inherit pkgs; },
+  mkWindowsApp ? erosanix.defaultNix.lib."${ builtins.currentSystem }".mkWindowsApp,
+  ...
 }:
 lib.filesystem.packagesFromDirectoryRecursive {
-  inherit (pkgs) callPackage newScope;
+  inherit (pkgs) callPackage;
+  newScope = extra: lib.callPackageWith (pkgs // extra // {
+    inherit mkWindowsApp;
+  });
   directory = ./pkgs;
 }
---
+++ pkgs/sseedit/package.nix
@@ -0,0 +1,64 @@
+{
+  requireFile,
+  mkWindowsApp,
+  lib,
+
+  wine,
+  p7zip,
+}:
+let
+  nexusArchive = requireFile {
+    # Original filename is 'SSEEdit 4.1.5f-164-4-1-5f-1714283656.7z', but the name may cannot
+    # contain a space.
+    name = "SSEEdit-4.1.5f-164-4-1-5f-1714283656.7z";
+    url = "https://www.nexusmods.com/skyrimspecialedition/mods/164";
+    hash = "sha256-8kgK8HYoZ5d4bDD6+PB3VyPJ6F8fBRCsEH9omIgbQPA=";
+  };
+in
+mkWindowsApp {
+  inherit wine;
+
+  pname = "sseedit";
+  version = "4.1.5f";
+
+  src = nexusArchive;
+  dontUnpack = true;
+
+  buildInputs = [ p7zip ];
+
+  enableMonoBootPrompt = false;
+  fileMap = {
+    "$HOME/.local/share/Steam/steamapps/common/Skyrim Special Edition/Data" =
+      "drive_c/sseedit/Skyrim Special Edition Data";
+    "$HOME/.local/share/Steam/steamapps/compatdata/489830/pfx/drive_c/users/steamuser/Documents/My Games/Skyrim Special Edition" =
+      "drive_c/sseedit/Skyrim Special Edition Savedata";
+    "$HOME/.local/share/Steam/steamapps/compatdata/489830/pfx/drive_c/users/steamuser/AppData/Local/Skyrim Special Edition/Plugins.txt" =
+      "drive_c/sseedit/Plugins.txt";
+  };
+  winAppInstall = ''
+    install_dir="$WINEPREFIX/drive_c/sseedit"
+
+    mkdir -p "$install_dir"
+    7z x "-o$install_dir" "${nexusArchive}"
+  '';
+
+  winAppRun = ''
+    wine "$WINEPREFIX/drive_c/sseedit/SSEEdit 4.1.5f/SSEEdit.exe" \
+      -D:'C:\sseedit\Skyrim Special Edition Data' \
+      -M:'C:\sseedit\Skyrim Special Edition Savedata\' \
+      -P:'C:\sseedit\Plugins.txt' \
+      "$ARGS"
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    ln -s $out/bin/.launcher $out/bin/sseedit
+
+    runHook postInstall
+  '';
+
+  meta = {
+    sourceProvenance = [ lib.sourceTypes.binaryNativeCode ];
+  };
+}
